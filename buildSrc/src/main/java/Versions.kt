0ce06e0a787b2f05e0227900dfd04462e30a2ac3
Austin <austinchristensen4@gmail.com>
2020-12-02 09:58:25 -0700

    Working on this branch

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val androidxRecyclerView  = "1.1.0"
     const val coroutines = "1.2.1"
     const val constraintLayout = "1.1.3"
-    const val exoplayer = "2.12.2"
+    const val exoplayer = "2.11.8"
     const val gradle = "3.6.1"
     const val koin = "2.1.3"
     const val kotlin = "1.3.61"
